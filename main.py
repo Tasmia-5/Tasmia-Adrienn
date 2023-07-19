14f9efc4ff175a0091817ae25602416bc631f37b
Adrienn Komlodi <akomlodi@ufl.edu>
2023-07-19 18:02:59 -0400

    added decode function and finished option 2

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ def pass_encoder(password):
         print("Error: Password should be 8 digits long and contain only integers.")
         return None
 
+def pass_decoder(encoded_password):
+    decoded_pass = ""
+    pass_list = list(encoded_password)
+    for char in pass_list:
+        decoded_pass_digit = int(char) - 3
+        decoded_pass_digit = str(decoded_pass_digit)
+        decoded_pass += decoded_pass_digit
+    return decoded_pass
+
 
 if __name__ == "__main__":
     encoded_password_attempt = ""
@@ -23,6 +32,6 @@ if __name__ == "__main__":
             if encoded_password_attempt:
                 print("Your password has been encoded and stored!")
         elif option == 2:
-            pass
+            print(f"The encoded password is {encoded_password_attempt}, and the original password is {pass_decoder(encoded_password_attempt)}")
         elif option == 3:
             running = False
