24f1a5e4a1e6813586949eeaba68f20b2eaf83af
Tasmia-5 <128504724+Tasmia-5@users.noreply.github.com>
2023-07-19 17:31:59 -0400

    Correct File

---
+++ main.py
@@ -0,0 +1,27 @@
+def pass_encoder(password):
+    if password.isdigit() and len(password) == 8:
+        encoded_password = ''.join(str((int(char) + 3) % 10) for char in password)
+        return encoded_password
+    else:
+        print("Error: Password should be 8 digits long and contain only integers.")
+        return None
+
+
+if __name__ == "__main__":
+    encoded_password_attempt = ""
+    running = True
+    while running:
+
+        # Menu
+        print("\nMenu\n-------------\n1. Encode\n2. Decode\n3. Quit")
+        option = int(input("\nPlease enter an option: "))
+
+        if option == 1:
+            pass_entry = input("Please enter your password to encode: ")
+            encoded_password_attempt = pass_encoder(pass_entry)
+            if encoded_password_attempt:
+                print("Your password has been encoded and stored!")
+        elif option == 2:
+            pass
+        elif option == 3:
+            running = False
